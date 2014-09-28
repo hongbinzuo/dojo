d3f473fea0a5df8995d58fbf8c21f4ab4112f99b
ehonzuo <hongbinzuo@163.com>
2014-09-28 20:26:13 +0800

    add

--- src/com/freelemon/BubbleSort.java
+++ src/com/freelemon/BubbleSort.java
@@ -15,9 +15,7 @@ public class BubbleSort {
         for(int i=0;i<a.length-1;i++){
             for(int j=0;j<a.length-1-i;j++){
                 if(a[j]>a[j+1]){
-                    temp=a[j];
-                    a[j]=a[j+1];
-                    a[j+1]=temp;
+                    Util.exchange(a, j, j+1);
                 }
             }
         }
